44451abb49596599c58968bdf70b23bf64fef450
Maytecalvert <166538878+Maytecalvert@users.noreply.github.com>
2025-04-11 09:00:53 -0300

    CAMBIOS

---
+++ Assets/Script/tresgustos.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tresgustos : MonoBehaviour
+{
+    public string saborHelado;
+    public int cantidadGramos;
+    int costoGramo = 1250 / 1000;
+    float descuento = 0.9f;
+    float precioFinal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (cantidadGramos < 250 || cantidadGramos > 3000)
+        {
+            Debug.Log("ERROR, CANTIDAD MAYOR O MENOR A LA ESPERADA");
+            return;
+        }
+        if (saborHelado != "CHO"  && saborHelado != "FRU" && saborHelado != "DDL")
+        {
+            Debug.Log("SABOR NO DISPONIBLE");
+            return;
+        }
+
+        if (saborHelado == "CHO" || saborHelado == "DDL")
+        {
+            precioFinal = costoGramo * cantidadGramos;
+        } else if (saborHelado == "FRU")
+        {
+            precioFinal = costoGramo * (cantidadGramos * descuento);
+        }
+        Debug.Log("PRECIO TOTAL:" + precioFinal);
+    }
+// Update is called once per frame
+void Update()
+    {
+        
+    }
+}
+
